1b2c55eb5d966c6166f2fb376f360dd53be3c981
Hanics Mihály Péter <hanicsm@gmail.com>
2023-04-30 06:04:50 +0200

    Finishing the script

--- RUNSCRIPT.R
+++ RUNSCRIPT.R
@@ -1,25 +1,46 @@
 install.packages("here")
-install.packages("testthat")
-#("C:/InterviewTask/Data/scenario_FE_SHAPE-final.RData")
+
 library(here) # for easy and clear relative paths
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) # set work directory to this file
 here::i_am("TaskReadme.txt")
-library(data.table)
-#library(reshape2)
-library(tidyverse)
-library(RUnit)
+
+library(readr) #For storing large data into .csv files
 
 source("utils.R") # common utility functions for this repository
 source("calculator_utils.R") # specific model utility functions
 
-scenario <- readRDS("Data/scenario_FE_SHAPE-final.Rdata")
-dle <- readRDS("Data/projected_dle-total-and-sectoral-scaled_SHAPE-final.Rdata")
-energy <- readRDS("Data/projected_energy_inequality_SHAPE-final.Rdata")
+########################Solution##########################
 
-comb12 = full_join(dle, scenario)
-combined = full_join(comb12,energy)
-combined %>% group_by(model, scenario, iso, variable, year) %>% dplyr::summarize(model = paste(na.omit(model)), scenario = paste(na.omit(scenario)), iso = paste(na.omit(iso)), variable = paste(na.omit(variable)), year = paste(na.omit(year)))
+IAM.OUTPUT.SHORT.FILENAME = "SHAPE-final"  #Appropriate name for the calculation
+source("calculator_output_lognormal-calculation.R")
+#Now the data is stored in two environmental variables: scenario.assessment.data, scenario.assessment.calculated
 
-capture.output(combined, file = "output.csv")
-capture.output(combined, file = "output_txt.txt")
+#Print out the "untouched" data (this is not neccessary, because the calculated list contains every information the raw data contains)
+write_csv(scenario.assessment.data, "ScenarioDataOutputCSV.csv")
+save(scenario.assessment.data, file = "ScenarioDataOutputR.RData")
+
+#Print out the calculated data
+write_csv(scenario.assessment.calculated, "CalculatedOutputCSV.csv")
+save(scenario.assessment.calculated, file = "CalculatedOutputR.RData")
+
+
+#################Original "solution" (working using the wrong files)###################
+
+#library(data.table)
+#library(reshape2)
+#library(tidyverse)
+#
+##1) Read the data, store it as lists
+#scenario <- readRDS("Data/scenario_FE_SHAPE-final.Rdata")
+#dle <- readRDS("Data/projected_dle-total-and-sectoral-scaled_SHAPE-final.Rdata")
+#energy <- readRDS("Data/projected_energy_inequality_SHAPE-final.Rdata")
+#
+##2) Combine them into one superlist
+#comb12 = full_join(dle, scenario)
+#combined = full_join(comb12,energy)
+#combined %>% group_by(model, scenario, iso, variable, year) %>% dplyr::summarize(model = paste(na.omit(model)), scenario = paste(na.omit(scenario)), iso = paste(na.omit(iso)), variable = paste(na.omit(variable)), year = paste(na.omit(year)))
+#
+##3) Create the output
+#capture.output(combined, file = "output.csv")
+#capture.output(combined, file = "output_txt.txt")
 
--- UnitTest.R
+++ UnitTest.R
@@ -1,5 +1,16 @@
+install.packages("RUnit")
+install.packages("testthat") #For unit testing
+
+
+
 library(here) # for easy and clear relative paths
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) # set work directory to this file
+here::i_am("TaskReadme.txt")
+
+library(RUnit) #For unit testing
 library(testthat)
+
+source("RUNSCRIPT.R")
 
 readingFile <- function(filename){
   read.csv(filename)
@@ -7,5 +18,5 @@ readingFile <- function(filename){
 
 
 test_that("Our Data", {
-  expect_false(expect_null(readingFile("output.csv"), 0))
+  expect_false(expect_null(readingFile("output.csv")))
 })
